b3a3ca2d32ce396a1d7082e4db9869c0155db6cf
mamamTampan <138420489+mamamTampan@users.noreply.github.com>
2024-01-02 09:54:45 +0700

    Test for commit

---
+++ BalanceBracket/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BalancedBracket
+{
+	static string IsBalanced(string expression)
+	{
+		Stack<char> stack = new Stack<char>();
+		Dictionary<char, char> brackets = new Dictionary<char, char>
+		{
+			{'(', ')'},
+			{'[', ']'},
+			{'{', '}'}
+		};
+
+		foreach (char c in expression)
+		{
+			if (brackets.ContainsKey(c))
+			{
+				stack.Push(c);
+			}
+			else if (brackets.ContainsValue(c))
+			{
+				if (stack.Count == 0 || brackets[stack.Pop()] != c)
+				{
+					return "NO";
+				}
+			}
+		}
+
+		return stack.Count == 0 ? "YES" : "NO";
+	}
+
+	static void Main()
+	{
+		string sample1 = "{[()]}";
+		string sample2 = "{ [ ( ] ) }";
+		string sample3 = "{( ( [] ) [] ) []}";
+
+		Console.WriteLine($"Sample 1:\nInput: {sample1}\nOutput: {IsBalanced(sample1)}");
+		Console.WriteLine($"Sample 2:\nInput: {sample2}\nOutput: {IsBalanced(sample2)}");
+		Console.WriteLine($"Sample 3:\nInput: {sample3}\nOutput: {IsBalanced(sample3)}");
+	}
+}
---
+++ WeightedStrings/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+class WeightedStrings
+{
+    static void Main()
+    {
+        string s = "abbcccd";
+        List<int> queries = new List<int> { 1, 3, 9, 8 };
+
+        List<string> substrings = GenerateSubstrings(s);
+        List<int> weights = CalculateWeights(substrings);
+
+        List<string> results = CheckQueries(queries, weights);
+
+        Console.WriteLine("Output: [" + string.Join(", ", results) + "]");
+    }
+
+    static List<string> GenerateSubstrings(string s)
+    {
+        List<string> substrings = new List<string>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            char currentChar = s[i].ToString()[0];
+            string substring = currentChar.ToString();
+
+            for (int j = i + 1; j < s.Length; j++)
+            {
+                if (s[j] == currentChar)
+                {
+                    substring += currentChar;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            substrings.Add(substring);
+        }
+
+        return substrings;
+    }
+
+    static List<int> CalculateWeights(List<string> substrings)
+    {
+        List<int> weights = new List<int>();
+        foreach (string substring in substrings)
+        {
+            int weight = 0;
+            foreach (char c in substring)
+            {
+                weight += (int)(c - 'a') + 1;
+            }
+            weights.Add(weight);
+        }
+
+        return weights;
+    }
+
+    static List<string> CheckQueries(List<int> queries, List<int> weights)
+    {
+        List<string> results = new List<string>();
+        foreach (int query in queries)
+        {
+            if (weights.Contains(query))
+            {
+                results.Add("Yes");
+            }
+            else
+            {
+                results.Add("No");
+            }
+        }
+
+        return results;
+    }
+}
